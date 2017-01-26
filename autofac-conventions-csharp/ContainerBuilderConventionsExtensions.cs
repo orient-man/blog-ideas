afcabaabd3a54ed90c06173203b0cad8d472d166
orientman <orientman@gmail.com>
2017-01-26 17:41:22 +0100

    autofac-conventions-csharp

---
+++ autofac-conventions-csharp/ContainerBuilderConventionsExtensions.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using Autofac;
+using Autofac.Builder;
+using Autofac.Features.Scanning;
+using AutoMapper;
+
+namespace App.Common.Infrastructure
+{
+    public static class ContainerBuilderConventionsExtensions
+    {
+        public static ContainerBuilder RegisterWithConventions(
+            this ContainerBuilder builder,
+            Assembly assembly,
+            Action<IRegistrationBuilder<object, ScanningActivatorData, DynamicRegistrationStyle>>
+                lifetimeScope = null)
+        {
+            var setLifetimeScope = lifetimeScope ?? (o => o.InstancePerLifetimeScope());
+
+            // all AutoMapper profiles as singletons
+            builder.RegisterAssemblyTypes(assembly)
+                .Where(t => t.IsSubclassOf(typeof(Profile)))
+                .As<Profile>()
+                .SingleInstance();
+
+            // all top level classes without interfaces excluding mocks/stubs
+            setLifetimeScope(
+                builder.RegisterAssemblyTypes(assembly)
+                    .Where(t => !t.GetInterfaces().Any())
+                    .Where(t => !t.IsNested)
+                    .Where(t => !t.Name.EndsWith("Mock"))
+                    .Where(t => !t.Name.EndsWith("Stub"))
+                    .AsSelf());
+
+            // all top level classes with interfaces excluding mocks/stubs
+            setLifetimeScope(
+                builder.RegisterAssemblyTypes(assembly)
+                    .Where(t => !t.IsNested)
+                    .Where(t => !t.Name.EndsWith("Mock"))
+                    .Where(t => !t.Name.EndsWith("Stub"))
+                    .Where(t => !t.IsSubclassOf(typeof(Profile)))
+                    .AsImplementedInterfaces());
+
+            return builder;
+        }
+    }
+}
