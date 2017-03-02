b2adf9d8ffd4620cc4a98059128ff0378738467c
orientman <orientman@gmail.com>
2017-03-02 16:01:41 +0100

    binary-search-ex-csharp

---
+++ binary-search-ex-csharp/BinarySearch.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace App
+{
+    public static class BinarySearch
+    {
+        public static T FirstOrDefault<T>(
+            IReadOnlyList<T> array,
+            Func<T, NextStep> pred)
+        {
+            var min = 0;
+            var max = array.Count - 1;
+            var candidate = default(T);
+            while (min <= max)
+            {
+                var mid = (min + max) / 2;
+                var value = array[mid];
+
+                switch (pred(value))
+                {
+                    case NextStep.FoundAndStop:
+                        return value;
+
+                    case NextStep.FoundAndGoLeft:
+                        candidate = value;
+                        max = mid - 1;
+                        break;
+                    case NextStep.GoLeft:
+                        max = mid - 1;
+                        break;
+                    case NextStep.FoundAndGoRight:
+                        candidate = value;
+                        min = mid + 1;
+                        break;
+                    case NextStep.GoRight:
+                        min = mid + 1;
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+            }
+
+            return candidate;
+        }
+
+        public enum NextStep
+        {
+            FoundAndStop,
+            FoundAndGoLeft,
+            FoundAndGoRight,
+            GoLeft,
+            GoRight
+        }
+    }
+}
---
+++ binary-search-ex-csharp/README.md
@@ -0,0 +1,37 @@
+﻿# Binary Search Extended
+
+For example supports finding _first_ greater or equal than x, or _last_ less or equal.
+
+Usage:
+
+    public int TryFindLastLessOrEqual(int[] values, int x)
+    {
+        return BinarySearch.FirstOrDefault(
+            values,
+            v => v <= x ? BinarySearch.NextStep.FoundAndGoRight : BinarySearch.NextStep.GoLeft);
+    }
+
+    public int TryFindLastGreaterOrEqual(int[] values, int x)
+    {
+        return BinarySearch.FirstOrDefault(
+            values,
+            v => v >= x ? BinarySearch.NextStep.FoundAndGoLeft : BinarySearch.NextStep.GoRight);
+    }
+
+    public int TryFind(int[] values, int x)
+    {
+        return BinarySearch.FirstOrDefault(
+            values,
+            v =>
+            {
+                switch (v.CompareTo(x))
+                {
+                    case 0:
+                        return BinarySearch.NextStep.FoundAndStop;
+                    case -1:
+                        return BinarySearch.NextStep.GoRight;
+                    default:
+                        return BinarySearch.NextStep.GoLeft;
+                }
+            });
+    }
